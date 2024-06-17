96b22b35cc9e38955e23db8dcdc3759451faa312
nrbrt2002 <nrbrt2002@gmail.com>
2024-06-17 14:10:43 +0200

    After adding administrator login and dashboard-> next to ceating functionalities

--- App/urls.py
+++ App/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    
+    path('admin-dashboard', views.admin_dashboard, name = "admin-dashboard"),
+    path('dashboard/center', views.center, name="center"),
+    # path('login', views.login, name="login"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
--- App/views.py
+++ App/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def login(request):
+   return render(request, 'login.html')
+
+
+@login_required
+def admin_dashboard(request):
+    return render(request, 'admin-dashboard.html')
+
+@login_required
+def center(request):
+   return render(request, 'center.html')
